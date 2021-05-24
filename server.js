8de2aedf539cb3217e657e3b01240a4aaa26b5e4
Arif Zain <iamarifzain@outlook.com>
2021-05-24 15:30:38 +0500

    cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const stripe = require('stripe')(
   'sk_test_51IsoKoC4yLgdhwCefALIiyedM60Bc9znfSEpDFxUM2dgMmnSZmfMWbMuiyGgunOfmRNBlyjbrJSqClA8Smt4c1lH00xnVaSBLV',
